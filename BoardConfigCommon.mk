22a22133f60e8f97b830acfaf21878e0263927b4
itsxrp <itsxrproms@gmail.com>
2022-10-05 07:00:14 +0000

    sm8350-common: Set SELINUX_IGNORE_NEVERALLOWS to true
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,6 +196,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
